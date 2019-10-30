6a21b9cf5429c62ba1a962bd586172201dadb131
Abdul-fatai <staticdev20046@gmail.com>
2019-10-30 21:36:51 +0100

    delete post functionality added

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ addEventListener('DOMContentLoaded', getPosts);
 // Get form Data
 document.querySelector(".post-submit").addEventListener('click', submitData);
 
+// Delete Post
+document.querySelector("#posts").addEventListener('click', deletePost);
+
+
 
 function getPosts(){
   http.get("http://localhost:3000/posts")
@@ -30,6 +34,21 @@ function submitData(){
       getPosts();
     })
     .catch(err => console.log(err));
+}
+
+function deletePost(e){
+  e.prevenDefault();
+  if (e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if (confirm("Are you Sure?")) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+        .then(data => {
+          showAlert("Post removed", "alert alert-success");
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    }
+  }
 }
 
 
